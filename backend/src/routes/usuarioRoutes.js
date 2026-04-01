27933d903297350ace19436605ab9e78b29585d8
JSangaleti <julianosangaleti@alunos.utfpr.edu.br>
2026-04-01 15:22:53 -0300

    Alterado usuarioRoutes.js
    Acrescentadas as rotas do backend para realizar as operações no banco de dados (CRUD)

--- backend/src/routes/usuarioRoutes.js
+++ backend/src/routes/usuarioRoutes.js
@@ -15,6 +15,25 @@ router.get('/', usuarioController.listarUsuarios);
 
 /**
  * @swagger
+ * /usuarios/{id}:
+ *   get:
+ *     summary: Busca um usuário pelo ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       '200':
+ *         description: Usuário encontrado
+ *       '404':
+ *         description: Usuário não encontrado
+ */
+router.get('/:id', usuarioController.buscarUsuarioPorId);
+
+/**
+ * @swagger
  * /usuarios:
  *   post:
  *     summary: Cadastra um novo usuário
@@ -34,5 +53,54 @@ router.get('/', usuarioController.listarUsuarios);
  *         description: Usuário criado com sucesso
  */
 router.post('/', usuarioController.criarUsuario);
+
+/**
+ * @swagger
+ * /usuarios/{id}:
+ *   put:
+ *     summary: Atualiza um usuário existente
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               nome:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *     responses:
+ *       '200':
+ *         description: Usuário atualizado com sucesso
+ *       '404':
+ *         description: Usuário não encontrado
+ */
+router.put('/:id', usuarioController.atualizarUsuario);
+
+/**
+ * @swagger
+ * /usuarios/{id}:
+ *   delete:
+ *     summary: Remove um usuário
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       '200':
+ *         description: Usuário removido com sucesso
+ *       '404':
+ *         description: Usuário não encontrado
+ */
+router.delete('/:id', usuarioController.deletarUsuario);
 
 module.exports = router;
